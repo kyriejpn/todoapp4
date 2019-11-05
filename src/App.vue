439c6348b53cd7265e6049c1bf2b18fc43f4d449
kyrie2 <info@kyrie.cloud>
2019-11-05 17:22:17 +0900

    change use routerjs

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,2 @@
-<template>
-<div>
-<todo-form v-on:add="addTodo($event)"></todo-form>
-<todo-list v-bind:todos="todos" v-on:delete="deleteTodo($event)"></todo-list> </div> </template>
-<script> 
-import TodoForm from './components/TodoForm'
-import TodoList from './components/TodoList'
+<template> <div> <h3>My Todo</h3> <router-view></router-view> </div> </template>
 
- export default { data () {
-  return {
-   todos: []
-    }
-    },
-    components: {
-     'todo-form': TodoForm,
-     'todo-list': TodoList
-     },
-     methods: { 
-     addTodo (newTodo) {
-      this.todos.push(newTodo);
-      }, 
-      deleteTodo (i) {
-       this.todos.splice(i, 1);
-       }
-       }
-      } 
- </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,4 +1,2 @@
-<template> <div> <h5>Todo List</h5> <ul> <li v-for="(todo, i) in todos" v-bind:key="i"> {{ todo }} <button v-on:click="deleteTodo(i)">DEL</button> </li> </ul> </div> </template> <script> export default { props: ['todos'], methods: { deleteTodo (i) { this.$emit('delete', i); }} } </script>
-
-
-
+<template> <div> <button v-on:click="addTodo">ADD</button> <h5>Todo List</h5> <ul> <li v-for="(todo, i) in todos" v-bind:key="i"> {{ todo }} <button v-on:click="deleteTodo(i)">DEL</button> </li>
+</ul> </div> </template> <script> export default { data () { return { todos: [] } }, created () { this.todos = JSON.parse(localStorage.getItem('todos')) || []; }, methods: { deleteTodo (i) { this.todos.splice(i, 1); localStorage.setItem('todos', JSON.stringify(this.todos)); }, addTodo () { this.$router.push('/todos/add'); } } } </script>
