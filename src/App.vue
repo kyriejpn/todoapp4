1c27b56b9021169a50cf1db7a796d072476a22a3
kyrie2 <info@kyrie.cloud>
2019-11-05 15:44:57 +0900

    4-2 1コンポーネント形式　動きます。

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<div><h3> My Todo</h3>
+<input v-model="newTodo" placeholder="Input here...">
+<button v-on:click="addTodo()">ADD</button>
+<h5> Todo List</h5>
+<ul>
+<li v-for="(todo,i) in todos" v-bind:key="i">
+{{ todo }}
+<button v-on:click="deleteTodo(i)">DEL</button>
+</li></ul></div></template>
+<script> export default {data(){return{todos:[],newTodo:''}},
+methods:{addTodo(){if(this.newTodo==='')return;this.todos.push(this.newTodo);
+this.newTodo='';
+},deleteTodo(i){this.todos.splice(i,1);
+}}}</script>
